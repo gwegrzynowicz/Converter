905d5e3b566898a2f2f4362eaa0f9719419d6d48
Grzegorz Wegrzynowicz <greg.wegrzynowicz@gmail.com>
2019-10-25 00:09:25 -0500

    Delete BuildConfig.java

--- gen/x40240/grzegorz/wegrzynowicz/a5/app/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package x40240.grzegorz.wegrzynowicz.a5.app;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
